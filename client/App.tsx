263424b7a90cbda59994f4d6a27582076ec74994
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2023-11-03 11:45:35 +0530

    auth ui part created

--- client/App.tsx
+++ client/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import SignUp from '@views/auth/SignUp'; 
-
+import SignUp from '@views/auth/SignUp';
+import SignIn from '@views/auth/SignIn';
+import ForgettenPassword from '@views/auth/ForgettenPassword';
+import Verification from '@views/auth/Verification';
 
 const App = () => {
-  return <SignUp />;
+  return <Verification />;
 };
 export default App;
