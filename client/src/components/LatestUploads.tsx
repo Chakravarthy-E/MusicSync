91b87dca7a830c25c15b3e934e48c21cd7d3a4b6
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2023-11-11 21:48:41 +0530

    add to favorite,playlist model created

--- client/src/components/LatestUploads.tsx
+++ client/src/components/LatestUploads.tsx
@@ -4,12 +4,16 @@ import {useFetchLatestAudios} from 'src/hooks/query';
 import PulseAnimationContainer from '@ui/PulseAnimationContainer';
 import colors from '@utils/colors';
 import AudioCard from '@ui/AudioCard';
+import {AudioData} from 'src/@types/audio';
 
-interface Props {}
+interface Props {
+  onAudioPress(item: AudioData, data: AudioData[]): void;
+  onAudioLongPress(item: AudioData, data: AudioData[]): void;
+}
 
 const dummyData = new Array(4).fill('');
 
-const LatestUploads: FC<Props> = props => {
+const LatestUploads: FC<Props> = ({onAudioPress, onAudioLongPress}) => {
   const {data, isLoading} = useFetchLatestAudios();
 
   if (isLoading)
@@ -32,7 +36,13 @@ const LatestUploads: FC<Props> = props => {
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         {data?.map(item => {
           return (
-            <AudioCard key={item.id} title={item.title} poster={item.poster} />
+            <AudioCard
+              key={item.id}
+              title={item.title}
+              poster={item.poster}
+              onPress={() => onAudioPress(item, data)}
+              onLongPress={() => onAudioLongPress(item, data)}
+            />
           );
         })}
       </ScrollView>
