b5d4fdfe6a21f554eb3dc764da190f7744a53484
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-02-05 21:06:37 +0530

    feature: like and card atoms added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,25 @@
-"use client"
+"use client";
+import Card from "@/components/atoms/Card/Card";
+import client, { getClient } from "@/utils/apiServices";
 import { getFromLocalStorage } from "@/utils/storage";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
   const router = useRouter();
+
+  const getLatestAudios = async () => {
+    try {
+      const response = await client.get("/audio/latest");
+      if (response.status === 200) {
+        setData(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const isAuthUser = getFromLocalStorage("AUTH_TOKEN");
   if (!isAuthUser) return router.push("/auth/login");
   return <main className="">Home</main>;
---
+++ frontend/src/components/atoms/LikeButton/LikeButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { FaHeart } from "react-icons/fa";
+import { useState } from "react";
+
+const LikeButton = () => {
+  const [isLiked, setIsLiked] = useState(false);
+  const handleIsLiked = () => {
+    setIsLiked((prev) => !prev);
+  };
+  return (
+    <button onClick={handleIsLiked} className={isLiked ? "text-blue-500" : ""}>
+      <FaHeart size={35} />
+    </button>
+  );
+};
+export default LikeButton;
