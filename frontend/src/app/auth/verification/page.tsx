3cf70d6ac7d5314fb467de55011f8237a26bebf2
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-02-05 15:01:23 +0530

    feature: otp field created

--- frontend/src/app/auth/verification/page.tsx
+++ frontend/src/app/auth/verification/page.tsx
@@ -1,4 +1,16 @@
+import { Button } from "@/components/atoms/Button/Button";
+import OTPField from "@/components/atoms/OTPField/OTPField";
+
 export default function Verification() {
-    return <div>Verification</div>;
-  };
-  
+  return (
+    <div className=" min-h-screen flex items-center justify-center flex-col space-y-9">
+      <div className=" flex items-center justify-center flex-col space-y-9 border px-10 py-10 h-96 w-96 rounded-md">
+        <h1 className=" text-xl font-Montserrat text-center font-semibold">
+          Verify your email by entering OTP
+        </h1>
+        <OTPField />
+        <Button variant="icon" buttonText="Submit" />
+      </div>
+    </div>
+  );
+}
