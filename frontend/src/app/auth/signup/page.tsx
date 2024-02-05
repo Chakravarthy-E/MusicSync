d7c4190509322d3b0b3dfacff48bdc88da31112f
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-02-05 10:07:33 +0530

    improve: intials completed

--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -1,66 +1,109 @@
 "use client";
-
+import { ChangeEventHandler, FC, useState } from "react";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/atoms/Button/Button";
 import { TextField } from "@/components/atoms/TextField/TextField";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import * as yup from "yup";
+import client from "@/utils/apiServices";
 
-export default function Login() {
-  const [singUpData, setSignUpData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
+interface Props {}
 
+const signupSchema = yup.object({
+  name: yup
+    .string()
+    .trim("Name is missing!")
+    .min(3, "Invalid name!")
+    .required("Name is required!"),
+  email: yup
+    .string()
+    .trim("Email is missing!")
+    .email("Invalid email!")
+    .required("Email is required!"),
+  password: yup
+    .string()
+    .trim("Password is missing!")
+    .min(8, "Password is too short!")
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/,
+      "Password is too simple!"
+    )
+    .required("Password is required!"),
+});
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+};
+
+const Signup: FC<Props> = (props) => {
+  const [signUpData, setSignUpData] = useState(initialValues);
   const router = useRouter();
-  const handleLogin = (e: Event) => {
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { name, value } = e.target;
+    setSignUpData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(signUpData);
+    const data = await client.post("/auth/create", {
+      ...signUpData,
+    });
+    try {
+    } catch (error) {}
   };
 
   return (
-    <div className=" min-h-screen flex items-center justify-center flex-col">
-      <div className="flex  flex-col justify-center space-y-3 space-x-3 font-Montserrat border px-10 py-10 rounded-md h-96">
-        <h1 className=" text-xl text-center text-blue-600 font-semibold underline">Sign Up</h1>
+    <div className="min-h-screen flex items-center justify-center flex-col">
+      <div className="flex  flex-col justify-center space-y-1 space-x-3 font-Montserrat border px-10 py-10 rounded-md h-96">
+        <h1 className="text-xl text-center text-blue-600 font-semibold underline">
+          Sign Up
+        </h1>
         <TextField
           label="Name"
           type="text"
           name="name"
-          inputDynamicClassName="w-96"
-          value={singUpData.name}
-          onChange={handleLogin}
+          inputDynamicClassName=" w-[20rem]"
+          value={signUpData.name}
+          onChange={handleInputChange}
         />
         <TextField
           label="Email"
           type="text"
           name="email"
-          inputDynamicClassName="w-96"
-          value={singUpData.email}
-          onChange={handleLogin}
+          inputDynamicClassName=" w-[20rem]"
+          value={signUpData.email}
+          onChange={handleInputChange}
         />
         <TextField
           label="Password"
           type="password"
-          name="email"
-          inputDynamicClassName=" w-96"
-          value={singUpData.password}
-          onChange={handleLogin}
+          name="password"
+          inputDynamicClassName=" w-[20rem]"
+          value={signUpData.password}
+          onChange={handleInputChange}
         />
-        <Button
-          variant="icon"
-          buttonText="Submit"
-          className=" flex items-center justify-center hover:text-blue-600"
-        />
-        <div className="text-center">
-          Already have account?{" "}
-          <button
-            className=" text-blue-600 text-base hover:underline"
-            onClick={()=>router.push("/auth/login")}
-          >
-            Login
-          </button>{" "}
-          here
+        <div className="text-center flex items-center justify-center flex-col space-y-4">
+          <Button variant="icon" buttonText="Submit" onClick={handleSubmit} />
+          <p>
+            Alredy have account?{" "}
+            <button
+              className=" text-blue-500   hover:text-blue-600 text-base hover:underline"
+              onClick={() => router.push("/auth/login")}
+            >
+              Login
+            </button>{" "}
+            here
+          </p>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Signup;
