f8968313609fcee8c1e64bee481fef00bad98d00
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-02-06 14:24:31 +0530

    impr: audio card improved

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,10 @@
 "use client";
-import Card from "@/components/atoms/Card/Card";
-import client, { getClient } from "@/utils/apiServices";
-import { getFromLocalStorage } from "@/utils/storage";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import LatestAudios from "@/components/molecules/LatestAudios/LatestAudios";
 
 export default function Home() {
-  const [data, setData] = useState([]);
-  const router = useRouter();
-
-  const getLatestAudios = async () => {
-    try {
-      const response = await client.get("/audio/latest");
-      if (response.status === 200) {
-        setData(response.data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const isAuthUser = getFromLocalStorage("AUTH_TOKEN");
-  if (!isAuthUser) return router.push("/auth/login");
-  return <main className="">Home</main>;
+  return (
+    <main className=" px-10 py-4">
+      <LatestAudios />
+    </main>
+  );
 }
