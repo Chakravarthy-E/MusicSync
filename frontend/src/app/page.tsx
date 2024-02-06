fdce156274fc71a68ed91fb556fb99d67aa33785
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-02-06 19:28:57 +0530

    feature: redux tool added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 import LatestAudios from "@/components/molecules/LatestAudios/LatestAudios";
+import { getFromLocalStorage } from "@/utils/storage";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const isAuthUser = getFromLocalStorage("AUTH_TOKEN");
+  if (!isAuthUser) return router.push("/auth/login");
+
   return (
     <main className=" px-10 py-4">
       <LatestAudios />
