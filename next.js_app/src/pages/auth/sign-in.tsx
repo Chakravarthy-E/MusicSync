446d25f09f01c8f4a2dd51ed6f88940a13830d07
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-02-21 17:02:10 +0530

    improve:sign-in and sign-up func completed

--- next.js_app/src/pages/auth/sign-in.tsx
+++ next.js_app/src/pages/auth/sign-in.tsx
@@ -1,30 +1,99 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import client from "@/utils/apiServices";
+
+const formSchema = z.object({
+  email: z.string().email("email required !"),
+  password: z.string().trim(),
+});
 
 const SignIn = () => {
   const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const response = await client.post("/auth/sign-in", values);
+    if (response.status === 200) {
+      toast({
+        title: "login success",
+        description: "successfully logged in",
+      });
+      router.push("/");
+    } else {
+      toast({
+        title: response.statusText,
+        description: "Account loggin failed",
+      });
+    }
+  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="my-4 text-3xl font-semibold tracking-wide">
         Login With Your Accout
       </h1>
-      <form className="my-3 flex w-96 flex-col space-y-2 border px-10 py-10 text-sm">
-        <label htmlFor="email">Email</label>
-        <input
-          placeholder="enter your email"
-          id="email"
-          type="email"
-          className="rounded border px-2 py-2"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          placeholder="enter your password"
-          id="password"
-          type="password"
-          className="rounded border px-2 py-2"
-        />
-        <button className="rounded bg-blue-500 py-2 text-white">Sign in</button>
-      </form>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="my-3 w-96 space-y-3 rounded border px-10 py-10 shadow"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="enter your email"
+                    {...field}
+                    className="rounded"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="enter your password"
+                    {...field}
+                    type="password"
+                    className="w-full rounded"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
       <p>
         Don't have account?&nbsp;
         <span
--- next.js_app/src/pages/auth/sign-up.tsx
+++ next.js_app/src/pages/auth/sign-up.tsx
@@ -1,38 +1,128 @@
+"use client";
+
 import React from "react";
 import { useRouter } from "next/router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import client from "@/utils/apiServices";
+
+const formSchema = z.object({
+  name: z.string().min(3, "Invalid name!"),
+  email: z.string().email("Invalid email!"),
+  password: z
+    .string()
+    .min(8, "Password is too short!")
+    .regex(
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/,
+      "Password is too simple!",
+    ),
+});
 
 const SignUp = () => {
   const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const response = await client.post("/auth/create", values);
+    if (response.status === 201) {
+      toast({
+        title: "Account created successfully",
+        description: "successfully created",
+      });
+      router.push("/auth/sign-in");
+    } else {
+      toast({
+        title: "Error creating account",
+        description: "Account creation failed",
+      });
+    }
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="my-4 text-3xl font-semibold tracking-wide">
         Create An Accout
       </h1>
-
-      <form className="flex w-96 flex-col space-y-2 border px-10 py-10 text-sm my-3">
-        <label htmlFor="name">Name</label>
-        <input
-          placeholder="enter your name"
-          id="name"
-          type="text"
-          className="rounded border px-2 py-2"
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          placeholder="enter your email"
-          id="email"
-          type="email"
-          className="rounded border px-2 py-2"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          placeholder="enter your password"
-          id="password"
-          type="password"
-          className="rounded border px-2 py-2"
-        />
-        <button className="rounded bg-blue-500 py-2 text-white">Sign in</button>
-      </form>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="my-3 w-96 space-y-3 rounded border px-10 py-10 shadow"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="enter your name"
+                    {...field}
+                    className="rounded"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="enter your email"
+                    {...field}
+                    className="rounded"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="enter your password"
+                    {...field}
+                    type="password"
+                    className="w-full rounded"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
       <p>
         Already have account?&nbsp;
         <span
