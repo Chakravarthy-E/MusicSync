efab7e2d79de6d80fdee53f5e1338db2f8bab97b
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-02-29 18:07:43 +0530

    feature: redux store added

--- next.js_app/src/components/molecules/latestAudios/LatestAudios.tsx
+++ next.js_app/src/components/molecules/latestAudios/LatestAudios.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import client, { apiList } from "@/utils/apiServices";
 
 interface Props {}
 
 const LatestAudios = (props: Props) => {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["latest-audios"],
-    queryFn: () => client.get(apiList.latestAudios),
-  });
-  console.log(data);
+
   return <div>Latest Audios</div>;
 };
 
