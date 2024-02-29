f0a6d89dfee6e19e6686ebd918e1ccd530afa035
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-02-29 18:07:43 +0530

    feature: redux store added

--- next.js_app/src/pages/profile.tsx
+++ next.js_app/src/pages/profile.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { getAuthState } from "@/lib/slices/authSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 interface Props {}
 
 const Profile = (props: Props) => {
-  return <div>Profile</div>;
+  const { profile } = useSelector(getAuthState);
+  return <div>profile</div>;
 };
 
 export default Profile;
