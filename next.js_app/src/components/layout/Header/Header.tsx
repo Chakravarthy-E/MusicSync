6146b1125c4fb54aad0bb91af2b4a288073bdcbf
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-02-29 12:44:39 +0530

    improve: logout func added

--- next.js_app/src/components/layout/Header/Header.tsx
+++ next.js_app/src/components/layout/Header/Header.tsx
@@ -1,21 +1,40 @@
 import React from "react";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
 import { CircleUserRound } from "lucide-react";
 import { Button } from "@/components/ui/button";
-
+import { toast } from "@/components/ui/use-toast";
 
 const Header = () => {
-
-
-
+  const router = useRouter();
+  const user = Cookies.get("token");
+  const handleLogout = () => {
+    Cookies.remove("token");
+    toast({
+      title: "Logged out Successfully",
+    });
+    router.push("/auth/sign-in");
+  };
   return (
     <div className="flex w-full items-center justify-between border-b px-5 py-3">
-      <div className="text-3xl font-bold">MusicSync</div>
-      <div className="flex items-center space-x-3">
-        <Button variant="link" >
-          Logout
-        </Button>
-        <CircleUserRound className="cursor-pointer hover:text-blue-500" />
+      <div
+        className="cursor-pointer text-3xl font-bold text-primary"
+        onClick={() => router.push("/")}
+      >
+        MusicSync
       </div>
+      {user && (
+        <div className="flex items-center space-x-3">
+          <Button variant={"link"} onClick={handleLogout}>
+            Logout
+          </Button>
+          <CircleUserRound
+            size={35}
+            className="cursor-pointer hover:text-primary"
+            onClick={() => router.push("/profile")}
+          />
+        </div>
+      )}
     </div>
   );
 };
