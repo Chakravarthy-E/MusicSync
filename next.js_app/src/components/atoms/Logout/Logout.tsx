ae0f695282645f71b7345ba266cfaecf1604199b
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-03-03 14:36:15 +0530

    feature: Logout functionality added

--- next.js_app/src/components/atoms/Logout/Logout.tsx
+++ next.js_app/src/components/atoms/Logout/Logout.tsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
+import { clearProfile } from "@/lib/slices/authSlice";
 import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
 import { Button } from "@/components/ui/button";
 import { toast } from "@/components/ui/use-toast";
+import { getClient } from "@/utils/apiServices";
 
 const Logout = () => {
+  const dispatch = useDispatch();
   const router = useRouter();
-  const handleLogout = () => {
-    Cookies.remove("token");
-    toast({
-      title: "Logged out Successfully",
-    });
-    router.push("/auth/sign-in");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogout = async (fromAll?: boolean) => {
+    setIsLoading(true);
+    const endpoint = "/auth/log-out?fromAll=" + (fromAll ? "yes" : "");
+    try {
+      const client = await getClient();
+      await client.post(endpoint);
+      await Cookies.remove("token");
+      dispatch(clearProfile());
+      router.push("/auth/sign-in");
+      toast({
+        title: "Logged out Successfully",
+      });
+      setIsLoading(false);
+    } catch (error) {
+      toast({
+        title: "Logged out Error",
+      });
+    }
   };
-  return <Button onClick={handleLogout}>Logout</Button>;
+  return (
+    <Button onClick={() => handleLogout(true)}>
+      {isLoading ? "Logging out..." : "Logout"}
+    </Button>
+  );
 };
 
 export default Logout;
