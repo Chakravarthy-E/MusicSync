de437fc805780ae870aae991ca84e90e4ab83ddd
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-03-04 08:31:53 +0530

    feature : recommened audios added

---
+++ next.js_app/src/components/molecules/RecommendedAudios/RecommendedAudios.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import AudioCard from "@/components/atoms/AudioCard/AudioCard";
+import LoadingAnimation from "@/components/atoms/LoadingAnimation/LoadingAnimation";
+import { useFetchRecommendedAudios } from "@/hooks/query";
+
+interface Props {}
+
+const RecommendedAudios = (props: Props) => {
+  const { data, isLoading } = useFetchRecommendedAudios();
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col px-5 py-3">
+        <h1 className="py-2 text-xl font-semibold">Recommended Audios</h1>
+        <div className="flex space-x-3">
+          {[...Array(4)].map((_, index) => (
+            <LoadingAnimation key={index} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="py-3 px-5">
+      <h1 className="py-2 text-xl font-semibold">Recommended Audios</h1>
+      <div className="grid gap-3 lg:grid-cols-5">
+        {data?.map((item) => (
+          <AudioCard
+            key={item.id}
+            title={item.title}
+            poster={item.poster}
+            about={item.about}
+            file={item.file}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedAudios;
